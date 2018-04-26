6197e05afce870171d3e44d86cb647963a599f9d
Olena Gromova <elena555gr@gmail.com>
2018-04-26 11:08:00 +0200

    Changed collecttion1 task and regexTaask5

--- src/main/java/CollectionsTask1.java
+++ src/main/java/CollectionsTask1.java
@@ -5,30 +5,26 @@ public class CollectionsTask1 {
     public static void main(String[] args) {
         List<Integer> myCollection = new ArrayList();
 
-        for(int i=0; i<10;i++){
-            myCollection.add((int)randomFill());
+        for (int i = 0; i < 10; i++) {
+            myCollection.add((int) randomFill());
         }
-            System.out.println(myCollection);
-            myCollection.removeIf(i-> i>20);
-            System.out.println(myCollection);
-
-            if (myCollection.size()<8){
-                for(int i=myCollection.size(); i<10;i++)
-                myCollection.add(0);
-            }
-                System.out.println(myCollection);
-                myCollection.add(2, 1);
-                myCollection.add(8,-3);
-                myCollection.add(5,-4);
-
-
-             for (int i=0;i <myCollection.size();i++){
-                    System.out.println("position "+ i + ", value " + myCollection.get(i));
-        }
-        Collections.sort(myCollection);
         System.out.println(myCollection);
-        }
+        myCollection.removeIf(i -> i > 20);
+        System.out.println(myCollection);
 
+        if (myCollection.size() > 7) {
+            myCollection.add(2, 1);
+            myCollection.add(8, -3);
+            myCollection.add(5, -4);
+
+
+            for (int i = 0; i < myCollection.size(); i++) {
+                System.out.println("position " + i + ", value " + myCollection.get(i));
+            }
+            Collections.sort(myCollection);
+            System.out.println(myCollection);
+        }
+    }
     public static double randomFill(){
         Random rand = new Random();
         int randomNum = rand.nextInt(100);
--- src/main/java/RegexTask5.java
+++ src/main/java/RegexTask5.java
@@ -13,14 +13,13 @@ public class RegexTask5 {
     public static void main(String[] args) {
 
 
-       String str = "$2000.00 and $0.45 equals to $2000.453.";
+       String str = "$200000 and $0.45 equals to $2000.45 and not $0.456, not $89,98 $df45.gf $89.89 $0.45 ";
 
-       String pattern = "(\\$[0-9]+).([0-9]{2})";
+       String pattern = "\\$[0-9]+(\\.[0-9]{0,2}[^0-9])";
        Pattern p = Pattern.compile(pattern);
        Matcher m = p.matcher(str);
        while (m.find()){
            System.out.println(m.group(0));
-
            }
        }
     }
