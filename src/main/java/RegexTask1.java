e5bb7dbda3f9208efe36428da40d1ab954b71bd8
Olena Gromova <elena555gr@gmail.com>
2018-05-10 17:53:53 +0200

    Changes to RegexTask1

--- src/main/java/RegexTask1.java
+++ src/main/java/RegexTask1.java
@@ -5,16 +5,11 @@ import java.io.InputStreamReader;
 public class RegexTask1 {
     public static void main(String[] args) {
         BufferedReader br = null;
-
         try {
-
             br = new BufferedReader(new InputStreamReader(System.in));
-
             while (true) {
-
-                System.out.print("Enter something : ");
+                System.out.print("Enter Surname, Name and Patronimic : ");
                 String input = br.readLine();
-
                 if ("q".equals(input)) {
                     System.out.println("Exit!");
                     System.exit(0);
@@ -23,17 +18,14 @@ public class RegexTask1 {
                 String surname = parts[0];
                 String name = parts[1];
                 String patronimic = parts[2];
-
                 char initial1 = name.charAt(0);
                 char initial2 = patronimic.charAt(0);
 
                 System.out.println(surname + " " + initial1 + " "+ initial2);
                 System.out.println(name);
                 System.out.println(name + " " + patronimic + " " + surname);
-
                 System.out.println("-----------\n");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -45,6 +37,5 @@ public class RegexTask1 {
                 }
             }
         }
-
     }
 }
